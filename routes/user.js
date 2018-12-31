48f6a38e594cc3a4513d12f88dd01b5c269e9ada
Samrawit Mulugeta <samrawitmulu88@gmail.com>
2018-12-31 08:49:57 -0800

    Using routes

---
+++ routes/user.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+
+var {mongoose} = require('.././db/mongoose');
+// var {Todo} = require('./models/todo');
+var {User} = require('.././models/user')
+var {authenticate} = require('.././middleware/authenticate')
+var {ObjectID} = require('mongodb')
+const jwt = require('jsonwebtoken')
+
+
+router.post('/admin', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password','firstName', 'lastName', 'phoneNo', 'age', 'sex','address']);
+    var user = new User(body);
+
+    user.save().then((user) => {
+        return user.generateAuthToken()
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+}) 
+
+router.post('/admin/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+            
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    })
+    
+    
+})
+module.exports = router;
